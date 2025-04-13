64b83b5f03b0d1521886172f6afa431ad5183ac5
kavya12-moger <kavyamoger99@gmail.com>
2025-04-13 13:18:34 +0000

    container config

---
+++ 01_basic.js/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=123;
+let accountEmail = "kavyamoger2.com";
+var accountPassword="11221122";
+city="mangalore";
+let name;
+
+//var is  not used frequently becauses is not a scope functional 
+
+accountEmail="chethan12";
+accountPassword="22112211";
+city="bangalor";
+ console.table([accountId,accountEmail,accountPassword,city,name]);
+
+
